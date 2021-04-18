6fb63ef0a67259a7f01afb8fe7c1aa9304e29046
Jim <dorajim15@gmail.com>
2021-04-18 19:04:48 +0800

    feat: add face recognition

---
+++ face_recognition/detector.py
@@ -0,0 +1,97 @@
+from typing import NamedTuple, List, Any
+import cv2
+import numpy as np
+import face_recognition
+import os
+import time
+
+IMAGE_FOLDER = 'images/'
+RED_COLOR = (200, 58, 76)
+WHITE_COLOR = (255, 255, 255)
+
+class Face(NamedTuple):
+    name: str
+    encode: Any
+
+class MatchResult(NamedTuple):
+    name: str
+    location: Any
+    unknown: bool
+
+
+def load_img_2_rgb(path):
+    img = cv2.imread(path)
+    return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+
+def load_faces() -> List[Face]:
+    faces = []
+    file_list = os.listdir(IMAGE_FOLDER)
+
+    for img_file in file_list:
+        img = load_img_2_rgb(f'{IMAGE_FOLDER}{img_file}')
+        encode = face_recognition.face_encodings(img)[0]
+
+        faces.append(Face(
+            name=img_file.split('.')[0],
+            encode=encode,
+        ))
+
+    return faces
+
+# img => RGB
+def draw_location(img, match_result, scale=1):
+    y1, x2, y2, x1 = match_result.location
+    y1, x2, y2, x1 = y1 * scale, x2 * scale, y2 * scale, x1 * scale
+    cv2.rectangle(img, (x1, y1), (x2, y2), RED_COLOR, 2)
+    cv2.rectangle(img, (x1, y2 - 35), (x2, y2), RED_COLOR, cv2.FILLED)
+    cv2.putText(img, match_result.name, (x1 + 10, y2-10), cv2.FONT_HERSHEY_COMPLEX, 0.8, WHITE_COLOR, 2)
+
+class FaceDetector:
+
+    def __init__(self):
+        print('loading known face encodes...')
+
+        _t = time.time()
+        self.faces = load_faces()
+
+        print(f'load finish!, use {time.time() - _t} seconds')
+
+    @property
+    def face_encodes(self):
+        if not hasattr(self, '_face_encodes'):
+            self._face_encodes = [f.encode for f in self.faces]
+
+        return self._face_encodes
+
+    def detect(self, img) -> List[MatchResult]:
+        results = []
+
+        cur_face_locs = face_recognition.face_locations(img)
+        cur_face_encodes = face_recognition.face_encodings(img, cur_face_locs)
+
+        for cur_face_loc, cur_face_encode in zip(cur_face_locs, cur_face_encodes):
+            matches = face_recognition.compare_faces(self.face_encodes, cur_face_encode)
+            face_dis = face_recognition.face_distance(self.face_encodes, cur_face_encode)
+
+            # print('matches', matches)
+            # print('face_dis', face_dis)
+
+            match_index = np.argmin(face_dis)
+            if matches[match_index]:
+                print('match name:', self.faces[match_index].name)
+
+                results.append(MatchResult(
+                    name=self.faces[match_index].name,
+                    location=cur_face_loc,
+                    unknown=False
+                ))
+
+            else:
+                results.append(MatchResult(
+                    name='',
+                    location=cur_face_loc,
+                    unknown=True
+                ))
+
+        return results
---
+++ face_recognition/test_img.py
@@ -0,0 +1,20 @@
+import sys
+import cv2
+
+from detector import FaceDetector, load_img_2_rgb, draw_location
+
+TEST_IMG_FOLDER = 'test_images/'
+
+def main(file_name):
+    test_img = load_img_2_rgb(TEST_IMG_FOLDER + file_name)
+    detector = FaceDetector()
+    results = detector.detect(test_img)
+    for r in results:
+        draw_location(test_img, r)
+
+    cv2.imshow('result', cv2.cvtColor(test_img, cv2.COLOR_RGB2BGR))
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main(sys.argv[1])
