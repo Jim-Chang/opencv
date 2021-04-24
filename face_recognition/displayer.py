602b2be3ed7394e41f7236511daf8d05cb43d201
Jim Chang <dorajim15@gmail.com>
2021-04-25 00:00:43 +0800

    fix: draw target

--- face_recognition/displayer.py
+++ face_recognition/displayer.py
@@ -10,8 +10,8 @@ def draw_locations(img, match_results, scale=1):
         y1, x2, y2, x1 = match_result.location
         y1, x2, y2, x1 = y1 * scale, x2 * scale, y2 * scale, x1 * scale
         cv2.rectangle(img, (x1, y1), (x2, y2), RED_COLOR, 2)
-        cv2.rectangle(img, (x1, y2 - 35), (x2, y2), RED_COLOR, cv2.FILLED)
-        cv2.putText(img, match_result.name, (x1 + 10, y2-10), cv2.FONT_HERSHEY_COMPLEX, 0.8, WHITE_COLOR, 2)
+        cv2.rectangle(img, (x1, y2 + 35), (x2, y2), RED_COLOR, cv2.FILLED)
+        cv2.putText(img, match_result.name, (x1 + 10, y2 + 25), cv2.FONT_HERSHEY_COMPLEX, 0.8, WHITE_COLOR, 2)
 
 # img => RGB
 def show_to_window(img, window='cam', wait=1):
