61fa49463e9c20671a50ad56ddac589bb8edd07e
Jim <dorajim15@gmail.com>
2021-06-13 15:50:52 +0800

    fix: multi thread web server

--- ros/src/ai_brain/ai_brain/web_control/serve.py
+++ ros/src/ai_brain/ai_brain/web_control/serve.py
@@ -5,8 +5,6 @@ from .web_server import app
 from .joystick_command_node import WebJoystickCommandNode
 from .image_receive_node import ImageReceiveNode
 
-def web_server():
-    app.run(host='0.0.0.0', port=1234)
 
 def main(args=None):
     rclpy.init(args=args)
@@ -16,11 +14,13 @@ def main(args=None):
     app.joystick_cmd_publisher = cmd_pub
     app.image_receiver = im_receive
 
-    t = threading.Thread(target=web_server)
+    t = threading.Thread(target=app.run, kwargs=dict(host='0.0.0.0', port=1234))
     t.start()
-    # app.run(host='0.0.0.0', port=1234)
 
-    rclpy.spin(im_receive)
+    try:
+        rclpy.spin(im_receive)
+    except KeyboardInterrupt:
+        print('stop ros node..., press ctrl+c again to stop web server')
 
     cmd_pub.destroy_node()
     im_receive.destroy_node()
