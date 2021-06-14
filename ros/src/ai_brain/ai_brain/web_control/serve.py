1e7048658315915d3ee083f352a41298f0ac31ea
Jim <dorajim15@gmail.com>
2021-06-14 10:43:34 +0800

    refactor: modify initial step at web server node

--- ros/src/ai_brain/ai_brain/web_control/serve.py
+++ ros/src/ai_brain/ai_brain/web_control/serve.py
@@ -8,20 +8,27 @@ from .image_receive_node import ImageReceiveNode
 
 def main(args=None):
     rclpy.init(args=args)
+
+    # prepare nodes
     cmd_pub = WebJoystickCommandNode()
     im_receive = ImageReceiveNode()
+
+    # start ros executor in thread
+    executor = rclpy.get_global_executor()
+    executor.add_node(cmd_pub)
+    executor.add_node(im_receive)
+    t = threading.Thread(target=executor.spin)
+    t.start()
     
+    # register node to flask app
     app.joystick_cmd_publisher = cmd_pub
     app.image_receiver = im_receive
 
-    t = threading.Thread(target=app.run, kwargs=dict(host='0.0.0.0', port=1234))
-    t.start()
+    # start flask app and stuck main thread
+    app.run(host='0.0.0.0', port=1234)
 
-    try:
-        rclpy.spin(im_receive)
-    except KeyboardInterrupt:
-        print('stop ros node..., press ctrl+c again to stop web server')
-
+    # destroy nodes and stop ros executor
+    print('stop ros nodes...')
     cmd_pub.destroy_node()
     im_receive.destroy_node()
     rclpy.shutdown()
