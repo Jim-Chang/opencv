20bd13e5f2c0a42e3fefbca8395a8ef72bcd717f
Jim Chang <dorajim15@gmail.com>
2021-06-20 17:19:21 +0800

    feat: add fullscreen

--- joystick/src/app/app.component.ts
+++ joystick/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component, ViewChild, OnInit, ElementRef } from '@angular/core';
+import { Component, ViewChild, OnInit, ElementRef, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 import { JoystickEvent, NgxJoystickComponent } from 'ngx-joystick';
 import { JoystickManagerOptions, JoystickOutputData } from 'nipplejs';
 import { DirectionService } from './services/direction.service';
@@ -8,10 +9,11 @@ import { DirectionService } from './services/direction.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'joystick';
 
   @ViewChild('semiJoystick') semiJoystick: NgxJoystickComponent;
+  private docElem: any;
 
   semiOptions: JoystickManagerOptions = {
     mode: 'dynamic',
@@ -23,8 +25,13 @@ export class AppComponent {
   semiOutputData: JoystickOutputData;
 
   recStatus = false;
+  private isFullscreen = false;
 
-  constructor(public directionService: DirectionService) {
+  constructor(public directionService: DirectionService, @Inject(DOCUMENT) private document: any) {
+  }
+
+  ngOnInit() {
+    this.docElem = document.documentElement;
   }
 
   onMotorMove(event: JoystickEvent) {
@@ -52,6 +59,42 @@ export class AppComponent {
   onClickRecBtn(): void {
     this.recStatus = !this.recStatus;
     this.directionService.sendRecCmd(this.recStatus);
+  }
+
+  onClickScreenBtn(): void {
+    this.isFullscreen = !this.isFullscreen;
+    return this.isFullscreen ? this.openFullscreen() : this.closeFullscreen();
+  }
+
+  private openFullscreen(): void {
+    if (this.docElem.requestFullscreen) {
+      this.docElem.requestFullscreen();
+    } else if (this.docElem.mozRequestFullScreen) {
+      /* Firefox */
+      this.docElem.mozRequestFullScreen();
+    } else if (this.docElem.webkitRequestFullscreen) {
+      /* Chrome, Safari and Opera */
+      this.docElem.webkitRequestFullscreen();
+    } else if (this.docElem.msRequestFullscreen) {
+      /* IE/Edge */
+      this.docElem.msRequestFullscreen();
+    }
+  }
+
+  /* Close fullscreen */
+  private closeFullscreen(): void {
+    if (this.document.exitFullscreen) {
+      this.document.exitFullscreen();
+    } else if (this.document.mozCancelFullScreen) {
+      /* Firefox */
+      this.document.mozCancelFullScreen();
+    } else if (this.document.webkitExitFullscreen) {
+      /* Chrome, Safari and Opera */
+      this.document.webkitExitFullscreen();
+    } else if (this.document.msExitFullscreen) {
+      /* IE/Edge */
+      this.document.msExitFullscreen();
+    }
   }
 
 }
