29f106c722137d6b27fdd10a296e142777e8e1ac
Jim <dorajim15@gmail.com>
2021-07-18 10:55:44 +0800

    feat: add launch files

---
+++ ros/src/ai_brain/launch/auto_drive.launch.py
@@ -0,0 +1,27 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='ros_deep_learning',
+            node_executable='video_source',
+            name='video_src_node',
+            arguments=['--ros-args', '-p', 'resource:="csi://0"', '-p', 'width:=640', '-p', 'height:=360', '-p', 'framerate:=30.']
+        ),
+        Node(
+            package='ai_brain',
+            node_executable='motor',
+            name='motor_node'
+        ),
+        Node(
+            package='ai_brain',
+            node_executable='web_server',
+            name='joystick_node'
+        ),
+        Node(
+            package='ai_brain',
+            node_executable='auto_drive',
+            name='auto_drive_node'
+        ),
+    ])
